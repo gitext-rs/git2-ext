4495cbbcb53be367c96f4a231af1c525cf60d75e
Ed Page <eopage@gmail.com>
2024-02-07 11:55:29 -0600

    test(bench): Switch to Divan

--- benches/ops.rs
+++ benches/ops.rs
@@ -1,4 +1,21 @@
-use std::collections::HashSet;
+#[divan::bench]
+fn bench_get_changed_paths_between_trees(bencher: divan::Bencher) {
+    let repo = get_repo();
+    let oid = repo.head().unwrap().target().unwrap();
+    let commit = repo.find_commit(oid).unwrap();
+    let parent = commit.parent(0).unwrap();
+    let parent_tree = parent.tree().unwrap();
+    let commit_tree = commit.tree().unwrap();
+
+    bencher.bench_local(|| {
+        git2_ext::tree::get_changed_paths_between_trees(
+            &repo,
+            Some(&parent_tree),
+            Some(&commit_tree),
+        )
+        .unwrap()
+    })
+}
 
 fn get_repo() -> git2::Repository {
     let repo_dir =
@@ -6,25 +23,6 @@ fn get_repo() -> git2::Repository {
     git2::Repository::discover(std::path::PathBuf::from(repo_dir)).unwrap()
 }
 
-fn bench_get_changed_paths_between_trees(c: &mut criterion::Criterion) {
-    c.bench_function("get_changed_paths_between_trees", |b| {
-        let repo = get_repo();
-        let oid = repo.head().unwrap().target().unwrap();
-        let commit = repo.find_commit(oid).unwrap();
-        let parent = commit.parent(0).unwrap();
-        let parent_tree = parent.tree().unwrap();
-        let commit_tree = commit.tree().unwrap();
-
-        b.iter(|| -> HashSet<std::path::PathBuf> {
-            git2_ext::tree::get_changed_paths_between_trees(
-                &repo,
-                Some(&parent_tree),
-                Some(&commit_tree),
-            )
-            .unwrap()
-        });
-    });
+fn main() {
+    divan::main();
 }
-
-criterion::criterion_group!(benches, bench_get_changed_paths_between_trees,);
-criterion::criterion_main!(benches);
